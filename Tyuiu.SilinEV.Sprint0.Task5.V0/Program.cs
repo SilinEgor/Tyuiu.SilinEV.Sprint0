89c6cf0a2da71603f45a29e9379abdf5c7ffc8f9
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-09-18 20:38:32 +0500

    Task5

---
+++ Tyuiu.SilinEV.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tyuiu.SilinEV.Sprint0.Task5.V0.Lib;
+
+Console.WriteLine("A + B = " + DataService.Additional(1, 5));
+Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+Console.WriteLine("A / B = " + DataService.Division(5, 0));
+Console.ReadKey();
