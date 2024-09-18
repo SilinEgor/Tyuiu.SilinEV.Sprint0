98724253b8fd9ff484bffd65ac7fcc5306e6f5a3
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-09-18 19:35:37 +0500

    Task3

---
+++ Tyuiu.SilinEV.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.SilinEV.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.SilinEV.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            Assert.AreEqual(10, DataServiceTest.Sum(5, 5));
+        }
+    }
+}
